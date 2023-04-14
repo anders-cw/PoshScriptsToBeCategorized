f257f3b15d03a0d60f6e66903736d83498138c50
anders-cw <115076797+anders-cw@users.noreply.github.com>
2023-04-14 16:36:20 +0200

    Add file

---
+++ translate.ps1
@@ -0,0 +1,11 @@
+﻿$text = Get-Clipboard
+
+$TargetLanguage = "en"
+$Uri = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=$($TargetLanguage)&dt=t&q=$($text.Replace('#',''))"
+$Response = Invoke-RestMethod -Uri $Uri -Method Get
+
+$Translation = $Response[0].SyncRoot | foreach { $_[0]+"`r`n" }
+
+write-host "##################"
+write-host $Translation
+write-host  "##################"
